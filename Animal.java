82de4d3817620777cc1a312c559b88d27fa61e63
Rafaela Santos <100327304+RafaelaSantos92@users.noreply.github.com>
2022-04-08 19:08:37 -0300

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,38 +1,28 @@
 package Familia51;
 
-public class Animal {
+public abstract class Animal {
 	
-	private String nome;
-	private int idade;
-	private String som;
-	
-	
-	public Animal(String nome,int idade,String som) 
-	{
-		super();
-		this.nome = nome;
-		this.idade = idade;
-		this.som = som;
+	private String especie;
 		
+	public Animal(String especie) 
+	{
+		this.setEspecie(especie);
 	}
-	public String getNome() {
-		return nome;
+	abstract public void nome(String nomeAnimal);
+	abstract public void idade(int idadeAnimal);
+	abstract public void som(String somAnimal);
+	
+	
+	public String getEspecie()
+	{
+			
+		return especie;
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
+	
+	public void setEspecie(String especie) {
+		this.especie = especie;
 	}
-	public int getIdade() {
-		return idade;
-	}
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
-	public String getSom() {
-		return som;
-	}
-	public void setSom(String som) {
-		this.som = som;
-	}
-
+	
+	
 
 }
